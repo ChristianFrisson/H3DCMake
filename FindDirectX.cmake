10f35261b301d97fba500ced23e01aec8386f365
dharani <dharani@deaff870-4328-4c97-957a-1a6c77341feb>
2017-02-27 12:20:47 +0000

    Fix for linker and architecture mapping for DirectX
    git-svn-id: https://h3dapi.org:8090/CMake/trunk/CMake@35 deaff870-4328-4c97-957a-1a6c77341feb

--- FindDirectX.cmake
+++ FindDirectX.cmake
@@ -11,17 +11,23 @@ find_path( DirectX_INCLUDE_DIR NAMES d3d9.h
            DOC "Path in which the file d3d9.h is located." )
 mark_as_advanced( DirectX_INCLUDE_DIR )
 
+set( proc_arch x86 )
+if( CMAKE_SIZEOF_VOID_P EQUAL 8 )
+ set( proc_arch  x64 )
+endif()
+
 # Look for the library.
 find_library( DirectX_d3d9_LIBRARY NAMES d3d9
-              PATHS $ENV{DXSDK_DIR}/Lib/$ENV{PROCESSOR_ARCHITECTURE}
+              PATHS $ENV{DXSDK_DIR}/Lib/${proc_arch}
               DOC "Path to d3d9 library." )
-
+ 
 find_library( DirectX_d3dx9_LIBRARY NAMES d3dx9
-              PATHS $ENV{DXSDK_DIR}/Lib/$ENV{PROCESSOR_ARCHITECTURE}
+              PATHS $ENV{DXSDK_DIR}/Lib/${proc_arch}
               DOC "Path to d3dx9 library." )
 
 mark_as_advanced( DirectX_d3d9_LIBRARY )
 mark_as_advanced( DirectX_d3dx9_LIBRARY )
+
 
 include( FindPackageHandleStandardArgs )
 # handle the QUIETLY and REQUIRED arguments and set DirectX_FOUND to TRUE
@@ -29,5 +35,5 @@ include( FindPackageHandleStandardArgs )
 find_package_handle_standard_args( DirectX DEFAULT_MSG
                                    DirectX_d3d9_LIBRARY DirectX_d3dx9_LIBRARY DirectX_INCLUDE_DIR )
 
-set( DirectX_LIBRARIES ${DirectX_LIBRARY} )
+set( DirectX_LIBRARIES ${DirectX_d3d9_LIBRARY} ${DirectX_d3dx9_LIBRARY} )
 set( DirectX_INCLUDE_DIRS ${DirectX_INCLUDE_DIR} )
