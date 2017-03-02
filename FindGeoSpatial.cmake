0a2ce4f7d95b61aeb31af29df21c2d5b4f237e85
markus <markus@deaff870-4328-4c97-957a-1a6c77341feb>
2017-03-02 15:44:48 +0000

    Parenthesis spaces formatting. that is cmake_command (something) -> cmake_command( something ) and endcmake_command( something ) -> endcmake_command()
    git-svn-id: https://h3dapi.org:8090/CMake/trunk/CMake@47 deaff870-4328-4c97-957a-1a6c77341feb

--- FindGeoSpatial.cmake
+++ FindGeoSpatial.cmake
@@ -5,14 +5,14 @@
 #  GeoSpatial_LIBRARIES    - List of libraries when using GeoSpatial.
 #  GeoSpatial_FOUND        - True if GeoSpatial found.
 
-get_filename_component(module_file_path ${CMAKE_CURRENT_LIST_FILE} PATH )
+get_filename_component( module_file_path ${CMAKE_CURRENT_LIST_FILE} PATH )
 
 # Look for the header file.
 find_path( GeoSpatial_INCLUDE_DIR NAMES H3D/GeoSpatial/GeoSpatial.h
            PATHS $ENV{H3D_ROOT}/../GeoSpatial/include
                  ../../../GeoSpatial/include
                  ${module_file_path}/../../../GeoSpatial/include )
-mark_as_advanced(GeoSpatial_INCLUDE_DIR)
+mark_as_advanced( GeoSpatial_INCLUDE_DIR )
 
 # Look for the library.
 if( MSVC )
@@ -21,21 +21,21 @@ if( MSVC )
   while( ${MSVC_VERSION} GREATER ${TEMP_MSVC_VERSION} )
     math( EXPR H3D_MSVC_VERSION "${H3D_MSVC_VERSION} + 1" )
     math( EXPR TEMP_MSVC_VERSION "${TEMP_MSVC_VERSION} + 100" )
-  endwhile( ${MSVC_VERSION} GREATER ${TEMP_MSVC_VERSION} )
+  endwhile()
   set( GeoSpatial_NAME "GeoSpatial_vc${H3D_MSVC_VERSION}" )
-elseif(UNIX)
+elseif( UNIX )
   set( GeoSpatial_NAME geospatial )
 else()
   set( GeoSpatial_NAME GeoSpatial )
-endif( MSVC )
+endif()
 
 set( DEFAULT_LIB_INSTALL "lib" )
 if( WIN32 )
   set( DEFAULT_LIB_INSTALL "lib32" )
   if( CMAKE_SIZEOF_VOID_P EQUAL 8 )
     set( DEFAULT_LIB_INSTALL "lib64" )
-  endif( CMAKE_SIZEOF_VOID_P EQUAL 8 )
-endif( WIN32 )
+  endif()
+endif()
 
 find_library( GeoSpatial_LIBRARY NAMES ${GeoSpatial_NAME}
               PATHS $ENV{H3D_ROOT}/../GeoSpatial/${DEFAULT_LIB_INSTALL}
@@ -44,48 +44,48 @@ find_library( GeoSpatial_LIBRARY NAMES ${GeoSpatial_NAME}
 find_library( GeoSpatial_DEBUG_LIBRARY NAMES ${GeoSpatial_NAME}_d
               PATHS $ENV{H3D_ROOT}/../GeoSpatial/${DEFAULT_LIB_INSTALL}
                     ../../../${DEFAULT_LIB_INSTALL} )
-mark_as_advanced(GeoSpatial_LIBRARY)
-mark_as_advanced(GeoSpatial_DEBUG_LIBRARY)
+mark_as_advanced( GeoSpatial_LIBRARY )
+mark_as_advanced( GeoSpatial_DEBUG_LIBRARY )
 
-if(GeoSpatial_LIBRARY OR GeoSpatial_DEBUG_LIBRARY)
+if( GeoSpatial_LIBRARY OR GeoSpatial_DEBUG_LIBRARY )
   set( HAVE_GeoSpatial_LIBRARY 1 )
-else(GeoSpatial_LIBRARY OR GeoSpatial_DEBUG_LIBRARY)
+else( GeoSpatial_LIBRARY OR GeoSpatial_DEBUG_LIBRARY )
   set( HAVE_GeoSpatial_LIBRARY 0 )
-endif(GeoSpatial_LIBRARY OR GeoSpatial_DEBUG_LIBRARY)
+endif()
 
 # Copy the results to the output variables.
-if(GeoSpatial_INCLUDE_DIR AND HAVE_GeoSpatial_LIBRARY)
+if( GeoSpatial_INCLUDE_DIR AND HAVE_GeoSpatial_LIBRARY )
 
-  set(GeoSpatial_FOUND 1)
-  if(GeoSpatial_LIBRARY)
-    set(GeoSpatial_LIBRARIES ${GeoSpatial_LIBRARIES} optimized ${GeoSpatial_LIBRARY} )
-  else(GeoSpatial_LIBRARY)
-    set(GeoSpatial_LIBRARIES ${GeoSpatial_LIBRARIES} optimized ${GeoSpatial_NAME} )
+  set( GeoSpatial_FOUND 1 )
+  if( GeoSpatial_LIBRARY )
+    set( GeoSpatial_LIBRARIES ${GeoSpatial_LIBRARIES} optimized ${GeoSpatial_LIBRARY} )
+  else( GeoSpatial_LIBRARY )
+    set( GeoSpatial_LIBRARIES ${GeoSpatial_LIBRARIES} optimized ${GeoSpatial_NAME} )
     message( STATUS "GeoSpatial release libraries not found. Release build might not work." )
-  endif(GeoSpatial_LIBRARY)
+  endif()
 
-  if(GeoSpatial_DEBUG_LIBRARY)
-    set(GeoSpatial_LIBRARIES ${GeoSpatial_LIBRARIES} debug ${GeoSpatial_DEBUG_LIBRARY} )
-  else(GeoSpatial_DEBUG_LIBRARY)
-    set(GeoSpatial_LIBRARIES ${GeoSpatial_LIBRARIES} debug ${GeoSpatial_NAME}_d )
+  if( GeoSpatial_DEBUG_LIBRARY )
+    set( GeoSpatial_LIBRARIES ${GeoSpatial_LIBRARIES} debug ${GeoSpatial_DEBUG_LIBRARY} )
+  else( GeoSpatial_DEBUG_LIBRARY )
+    set( GeoSpatial_LIBRARIES ${GeoSpatial_LIBRARIES} debug ${GeoSpatial_NAME}_d )
     message( STATUS "GeoSpatial debug libraries not found. Debug build might not work." )
-  endif(GeoSpatial_DEBUG_LIBRARY)
+  endif()
   
-  set(GeoSpatial_INCLUDE_DIR ${GeoSpatial_INCLUDE_DIR} )
-  set(GeoSpatial_LIBRARIES ${GeoSpatial_LIBRARIES} )
-else(GeoSpatial_INCLUDE_DIR AND HAVE_GeoSpatial_LIBRARY)
-  set(GeoSpatial_FOUND 0)
-  set(GeoSpatial_LIBRARIES)
-  set(GeoSpatial_INCLUDE_DIR)
-endif(GeoSpatial_INCLUDE_DIR AND HAVE_GeoSpatial_LIBRARY)
+  set( GeoSpatial_INCLUDE_DIR ${GeoSpatial_INCLUDE_DIR} )
+  set( GeoSpatial_LIBRARIES ${GeoSpatial_LIBRARIES} )
+else( GeoSpatial_INCLUDE_DIR AND HAVE_GeoSpatial_LIBRARY )
+  set( GeoSpatial_FOUND 0 )
+  set( GeoSpatial_LIBRARIES )
+  set( GeoSpatial_INCLUDE_DIR )
+endif()
 
 # Report the results.
-if(NOT GeoSpatial_FOUND)
-  set(GeoSpatial_DIR_MESSAGE
-    "GeoSpatial was not found. Make sure GeoSpatial_LIBRARY ( and/or GeoSpatial_DEBUG_LIBRARY ) and GeoSpatial_INCLUDE_DIR are set.")
-  if(GeoSpatial_FIND_REQUIRED)
-    message(FATAL_ERROR "${GeoSpatial_DIR_MESSAGE}")
-  elseif(NOT GeoSpatial_FIND_QUIETLY)
-    message(STATUS "${GeoSpatial_DIR_MESSAGE}")
-  endif(GeoSpatial_FIND_REQUIRED)
-endif(NOT GeoSpatial_FOUND)
+if( NOT GeoSpatial_FOUND )
+  set( GeoSpatial_DIR_MESSAGE
+    "GeoSpatial was not found. Make sure GeoSpatial_LIBRARY ( and/or GeoSpatial_DEBUG_LIBRARY ) and GeoSpatial_INCLUDE_DIR are set." )
+  if( GeoSpatial_FIND_REQUIRED )
+    message( FATAL_ERROR "${GeoSpatial_DIR_MESSAGE}" )
+  elseif( NOT GeoSpatial_FIND_QUIETLY )
+    message( STATUS "${GeoSpatial_DIR_MESSAGE}" )
+  endif()
+endif()
