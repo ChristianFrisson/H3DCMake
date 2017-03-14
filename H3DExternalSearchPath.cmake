1eebde05d783ede47587f488964f4a281a74b605
markus <markus@deaff870-4328-4c97-957a-1a6c77341feb>
2017-03-14 10:37:17 +0000

    H3D_EXTERNAL_BASE_DIR_NAME -> h3d_external_base_dir_name
    git-svn-id: https://h3dapi.org:8090/CMake/trunk/CMake@64 deaff870-4328-4c97-957a-1a6c77341feb

--- H3DExternalSearchPath.cmake
+++ H3DExternalSearchPath.cmake
@@ -7,34 +7,49 @@
 # exception though, for visual studio versions below vs2010 the vs2010 year string is looked for.
 set( check_if_h3d_external_matches_vs_version OFF )
 
+if( DEFINED H3D_EXTERNAL_BASE_DIR_NAME )
+  message( AUTHOR_WARNING "The variable 'H3D_EXTERNAL_BASE_DIR_NAME' is deprecated and no longer used. Please use the variable 'h3d_external_base_dir_name' instead which is most likely set in ${CMAKE_PARENT_LIST_FILE}" )
+endif()
+if( NOT DEFINED h3d_external_base_dir_name )
+  # This variable can be set after this file is included but before getExternalSearchPathsH3D function is called.
+  # If set to something and the compiles is not a supported MSVC version then it is expected that the setter of
+  # h3d_external_base_dir_name has setup an External directory in the same manner as for MSVC compilers but for
+  # their compiler version instead. The variable is expected to be global for the entire build.
+  if( DEFINED H3D_EXTERNAL_BASE_DIR_NAME )
+    set( h3d_external_base_dir_name ${H3D_EXTERNAL_BASE_DIR_NAME} )
+  else()
+    set( h3d_external_base_dir_name )
+  endif()
+endif()
+
 if( MSVC )
-  set( H3D_EXTERNAL_BASE_DIR_NAME "" )
+  set( h3d_external_base_dir_name )
   if( MSVC70 )
-    set( H3D_EXTERNAL_BASE_DIR_NAME vs2002 ) #This is probably not entirely correct but we won't support this anyways I would assume
+    set( h3d_external_base_dir_name vs2002 ) #This is probably not entirely correct but we won't support this anyways I would assume
   elseif( MSVC71 )
-    set( H3D_EXTERNAL_BASE_DIR_NAME vs2003 ) #This is probably not entirely correct but we won't support this anyways I would assume
+    set( h3d_external_base_dir_name vs2003 ) #This is probably not entirely correct but we won't support this anyways I would assume
   elseif( MSVC80 )
-    set( H3D_EXTERNAL_BASE_DIR_NAME vs2005 )
+    set( h3d_external_base_dir_name vs2005 )
   elseif( MSVC90 )
-    set( H3D_EXTERNAL_BASE_DIR_NAME vs2008 )
+    set( h3d_external_base_dir_name vs2008 )
   elseif( MSVC10 )
-    set( H3D_EXTERNAL_BASE_DIR_NAME vs2010 )
+    set( h3d_external_base_dir_name vs2010 )
   elseif( MSVC11 )
-    set( H3D_EXTERNAL_BASE_DIR_NAME vs2012 )
+    set( h3d_external_base_dir_name vs2012 )
   elseif( MSVC12 )
-    set( H3D_EXTERNAL_BASE_DIR_NAME vs2013 )
+    set( h3d_external_base_dir_name vs2013 )
   elseif( MSVC14 )
-    set( H3D_EXTERNAL_BASE_DIR_NAME vs2015 )
+    set( h3d_external_base_dir_name vs2015 )
   endif()
 
-  if( NOT H3D_EXTERNAL_BASE_DIR_NAME ) # fallback for future compilers, this assumes cmake version 3.0 and up.
+  if( NOT h3d_external_base_dir_name ) # fallback for future compilers, this assumes cmake version 3.0 and up.
     string( REGEX MATCH "[^ ]+$" vsYearString ${CMAKE_GENERATOR} )
     if( vsYearString )
       if( vsYearString STREQUAL "Win64" )
         string( REGEX REPLACE "([^ ]+) Win64$" "\\1" vsYearString ${CMAKE_GENERATOR} )
         string( REGEX MATCH "[^ ]+$" vsYearString ${vsYearString} )
       endif()
-      set( H3D_EXTERNAL_BASE_DIR_NAME vs${vsYearString} )
+      set( h3d_external_base_dir_name vs${vsYearString} )
     endif()
   endif()
 
@@ -50,7 +65,7 @@ function( checkIfValidH3DWinExternal arg1 arg2 )
   if( check_if_h3d_external_matches_vs_version )
     set( ${arg1} OFF PARENT_SCOPE )
     if( EXISTS ${arg2}/include/ACKNOWLEDGEMENTS )
-      file( STRINGS ${arg2}/include/ACKNOWLEDGEMENTS the_first_line LIMIT_COUNT 1 REGEX "[-][-][-][-] Compiled for ${H3D_EXTERNAL_BASE_DIR_NAME} [-][-][-][-]" ) # Check if first line contains the correct vsx string.
+      file( STRINGS ${arg2}/include/ACKNOWLEDGEMENTS the_first_line LIMIT_COUNT 1 REGEX "[-][-][-][-] Compiled for ${h3d_external_base_dir_name} [-][-][-][-]" ) # Check if first line contains the correct vsx string.
       if( the_first_line )
         set( ${arg1} ON PARENT_SCOPE )
       endif()
@@ -87,8 +102,8 @@ function( getExternalSearchPathsH3D arg1 arg2 arg3 )
         list( APPEND h3d_external_base_include_dirs ${h3d_ebd}/include )
         list( APPEND h3d_external_base_lib_dirs ${h3d_ebd}/${LIB} )
       endif()
-      if( H3D_EXTERNAL_BASE_DIR_NAME )
-        foreach( external_dir_name ${H3D_EXTERNAL_BASE_DIR_NAME} )
+      if( h3d_external_base_dir_name )
+        foreach( external_dir_name ${h3d_external_base_dir_name} )
           checkIfValidH3DWinExternal( add_dir ${h3d_ebd}/${external_dir_name} )
           if( add_dir )
             list( APPEND h3d_external_base_include_dirs ${h3d_ebd}/${external_dir_name}/include )
