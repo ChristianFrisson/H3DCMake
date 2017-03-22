680a500c5e5ba9b9e6f429a9e9a387b2042de5c5
markus <markus@deaff870-4328-4c97-957a-1a6c77341feb>
2017-03-22 13:26:36 +0000

    Fix to make sure that External is found before freetype-config. Hopefully we do not need to put the first section within IF( WIN32 ) case.
    git-svn-id: https://h3dapi.org:8090/CMake/trunk/CMake@76 deaff870-4328-4c97-957a-1a6c77341feb

--- FindFreetype.cmake
+++ FindFreetype.cmake
@@ -7,56 +7,15 @@
 
 include( H3DExternalSearchPath )
 
-find_program( FREETYPE_CONFIG_EXECUTABLE freetype-config
-      ONLY_CMAKE_FIND_ROOT_PATH
-      DOC "Path to freetype_config executable. Used to find freetype, not used on a standard Windows installation of H3DAPI." )
-mark_as_advanced( FREETYPE_CONFIG_EXECUTABLE )
-
-if( FREETYPE_CONFIG_EXECUTABLE )
-
-  # run the freetype-config program to get cflags
-  execute_process(
-        COMMAND sh "${FREETYPE_CONFIG_EXECUTABLE}" --cflags
-        OUTPUT_VARIABLE FREETYPE_CFLAGS
-        RESULT_VARIABLE return_value
-        ERROR_QUIET )
-
-  if( return_value EQUAL 0 )
-    if( ${CMAKE_MAJOR_VERSION} EQUAL 2 AND ${CMAKE_MINOR_VERSION} EQUAL 6 )
-      string( STRIP "${FREETYPE_CFLAGS}" FREETYPE_CFLAGS )
-    endif()
-    separate_arguments( FREETYPE_CFLAGS )
-
-    # parse definitions from cxxflags; drop -D* from CFLAGS
-    string( REGEX REPLACE "-D[^;]+;" ""
-            FREETYPE_CFLAGS "${FREETYPE_CFLAGS}" )
-
-    # parse include dirs from cxxflags; drop -I prefix
-    string( REGEX MATCHALL "-I[^;]+"
-            FREETYPE_INCLUDE_DIR_ft2build "${FREETYPE_CFLAGS}" )
-    string( REGEX REPLACE "-I[^;]+;" ""
-            FREETYPE_CFLAGS "${FREETYPE_CFLAGS}" )
-    string( REPLACE "-I" ""
-            FREETYPE_INCLUDE_DIR_ft2build "${FREETYPE_INCLUDE_DIR_ft2build}" )
-    string( REPLACE "\n" ""
-            FREETYPE_INCLUDE_DIR_ft2build "${FREETYPE_INCLUDE_DIR_ft2build}" )
-    set( FREETYPE_INCLUDE_DIR_ft2build "${FREETYPE_INCLUDE_DIR_ft2build}" )
-
-  endif()
-
-endif()
-
 get_filename_component( module_file_path ${CMAKE_CURRENT_LIST_FILE} PATH )
 getExternalSearchPathsH3D( module_include_search_paths module_lib_search_paths ${module_file_path} "freetype/include" "static" )
 
-if( NOT FREETYPE_INCLUDE_DIR_ft2build )
-  # Look for the header file.
-  find_path( FREETYPE_INCLUDE_DIR_ft2build NAMES freetype/freetype.h
-                                           PATHS ${module_include_search_paths}
-                                           DOC "Path in which the file freetype/freetype.h is located."
-                                           NO_SYSTEM_ENVIRONMENT_PATH )
-  mark_as_advanced( FREETYPE_INCLUDE_DIR_ft2build )
-endif()
+# Look for the header file.
+find_path( FREETYPE_INCLUDE_DIR_ft2build NAMES freetype/freetype.h
+                                         PATHS ${module_include_search_paths}
+                                         DOC "Path in which the file freetype/freetype.h is located."
+                                         NO_SYSTEM_ENVIRONMENT_PATH )
+mark_as_advanced( FREETYPE_INCLUDE_DIR_ft2build )
 
 # Look for the library.
 find_library( FREETYPE_LIBRARY NAMES freetype freetype2311 freetype2312MT freetype2312 freetype235
@@ -64,6 +23,50 @@ find_library( FREETYPE_LIBRARY NAMES freetype freetype2311 freetype2312MT freety
                                DOC "Path to freetype library."
                                NO_SYSTEM_ENVIRONMENT_PATH )
 mark_as_advanced( FREETYPE_LIBRARY )
+
+checkIfModuleFound( FREETYPE
+                    REQUIRED_VARS FREETYPE_INCLUDE_DIR_ft2build FREETYPE_LIBRARY )
+
+if( NOT FREETYPE_FOUND )
+  find_program( FREETYPE_CONFIG_EXECUTABLE freetype-config
+                ONLY_CMAKE_FIND_ROOT_PATH
+                DOC "Path to freetype_config executable. Used to find freetype, not used on a standard Windows installation of H3DAPI." )
+  mark_as_advanced( FREETYPE_CONFIG_EXECUTABLE )
+
+  if( FREETYPE_CONFIG_EXECUTABLE )
+
+    # run the freetype-config program to get cflags
+    execute_process(
+          COMMAND sh "${FREETYPE_CONFIG_EXECUTABLE}" --cflags
+          OUTPUT_VARIABLE FREETYPE_CFLAGS
+          RESULT_VARIABLE return_value
+          ERROR_QUIET )
+
+    if( return_value EQUAL 0 )
+      if( ${CMAKE_MAJOR_VERSION} EQUAL 2 AND ${CMAKE_MINOR_VERSION} EQUAL 6 )
+        string( STRIP "${FREETYPE_CFLAGS}" FREETYPE_CFLAGS )
+      endif()
+      separate_arguments( FREETYPE_CFLAGS )
+
+      # parse definitions from cxxflags; drop -D* from CFLAGS
+      string( REGEX REPLACE "-D[^;]+;" ""
+              FREETYPE_CFLAGS "${FREETYPE_CFLAGS}" )
+
+      # parse include dirs from cxxflags; drop -I prefix
+      string( REGEX MATCHALL "-I[^;]+"
+              FREETYPE_INCLUDE_DIR_ft2build "${FREETYPE_CFLAGS}" )
+      string( REGEX REPLACE "-I[^;]+;" ""
+              FREETYPE_CFLAGS "${FREETYPE_CFLAGS}" )
+      string( REPLACE "-I" ""
+              FREETYPE_INCLUDE_DIR_ft2build "${FREETYPE_INCLUDE_DIR_ft2build}" )
+      string( REPLACE "\n" ""
+              FREETYPE_INCLUDE_DIR_ft2build "${FREETYPE_INCLUDE_DIR_ft2build}" )
+      set( FREETYPE_INCLUDE_DIR_ft2build "${FREETYPE_INCLUDE_DIR_ft2build}" )
+
+    endif()
+
+  endif()
+endif()
 
 checkIfModuleFound( FREETYPE
                     REQUIRED_VARS FREETYPE_INCLUDE_DIR_ft2build FREETYPE_LIBRARY )
