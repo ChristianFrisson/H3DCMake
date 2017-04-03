e255cb62ff497e5d4da1519b7187517808045159
markus <markus@deaff870-4328-4c97-957a-1a6c77341feb>
2017-04-03 08:14:46 +0000

    case sensitive fix. Could potentially cause issues on some systems.
    git-svn-id: https://h3dapi.org:8090/CMake/trunk/CMake@83 deaff870-4328-4c97-957a-1a6c77341feb

--- FindWxWidgetsWin.cmake
+++ FindWxWidgetsWin.cmake
@@ -5,7 +5,7 @@
 #  wxWidgets_LIBRARIES    - List of libraries when using WxWidgets.
 #  wxWidgets_FOUND        - True if WxWidgets found.
 
-message( AUTHOR_WARNING "FindWxWidgetsWin.cmake is deprecated. Change from find_package( WxWidgetsWin ) or find_package( wxWidgetsWin ) to find_package( WxWidgets )." )
+message( AUTHOR_WARNING "FindWxWidgetsWin.cmake is deprecated. Change from find_package( WxWidgetsWin ) or find_package( wxWidgetsWin ) to find_package( wxWidgets )." )
 set( quiet_required_args )
 if( WxWidgetsWin_FIND_QUIETLY )
   set( quiet_required_args QUIET )
@@ -13,4 +13,4 @@ endif()
 if( WxWidgetsWin_FIND_REQUIRED )
   set( quiet_required_args ${quiet_required_args} REQUIRED )
 endif()
-find_package( WxWidgets ${quiet_required_args} )
+find_package( wxWidgets ${quiet_required_args} )
