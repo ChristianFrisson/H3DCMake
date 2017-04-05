d769806df81db4d90c88fef17002d7c63ae71219
markus <markus@deaff870-4328-4c97-957a-1a6c77341feb>
2017-04-05 11:29:20 +0000

    Added functions which can be used to setup rpath, handle common cache variables and settings GENERATE_CPACK_PROJECT, PREFER_STATIC_LIBRARIES CMAKE_INSTALL_PREFIX.
    git-svn-id: https://h3dapi.org:8090/CMake/trunk/CMake@86 deaff870-4328-4c97-957a-1a6c77341feb

--- H3DCommonFunctions.cmake
+++ H3DCommonFunctions.cmake
@@ -101,3 +101,139 @@ function( getDefaultH3DLibDirectory default_lib_directory_output )
   endif()
   set( ${default_lib_directory_output} ${default_lib_install} PARENT_SCOPE )
 endfunction()
+
+# Setup the desired RPath settings for H3D projects.
+function( setupRPathForLib )
+  # use, i.e. don't skip the full RPATH for the build tree
+  set( CMAKE_SKIP_BUILD_RPATH FALSE )
+
+  # when building, don't use the install RPATH already
+  # (but later on when installing)
+  set( CMAKE_BUILD_WITH_INSTALL_RPATH FALSE )
+
+  # Add the automatically determined parts of the RPATH
+  # which point to directories outside the build tree to the install RPATH
+  set( CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE )
+
+  # The RPATH to be used when installing, but only if it's not a system directory
+  list( FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" is_system_dir )
+  if( "${is_system_dir}" STREQUAL "-1" )
+     set( CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib" )
+  endif()
+endfunction()
+
+# Handles common cache variables for H3D projects. Some of which might be CMAKE variables.
+# Arguments are:
+# GENERATE_CPACK_PROJECT - Will be initialized to OFF
+# PREFER_STATIC_LIBRARIES - Will be initialized to OFF
+# CMAKE_INSTALL_PREFIX <prefix_path> - Will set CMAKE_INSTALL_PREFIX to the given path if it was not already initialized. 
+function( handleCommonCacheVar )
+  set( options GENERATE_CPACK_PROJECT PREFER_STATIC_LIBRARIES )
+  set( oneValueArgs CMAKE_INSTALL_PREFIX )
+  set( multiValueArgs )
+  include( CMakeParseArguments )
+  cmake_parse_arguments( setup_common_cache_var "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+  
+  if( setup_common_cache_var_CMAKE_INSTALL_PREFIX )
+    # set the install directory to the H3D directory on Windows
+    if( WIN32 AND CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT AND NOT H3D_CMAKE_INSTALL_PREFIX_ALREADY_SET )
+      set( CMAKE_INSTALL_PREFIX ${setup_common_cache_var_CMAKE_INSTALL_PREFIX} CACHE PATH "Install path prefix, prepended onto install directories." FORCE )
+      set( H3D_CMAKE_INSTALL_PREFIX_ALREADY_SET ON PARENT_SCOPE )
+    endif()
+  endif()
+
+  if( setup_common_cache_var_GENERATE_CPACK_PROJECT AND NOT DEFINED GENERATE_CPACK_PROJECT )
+    # Add a cache variable GENERATE_CPACK_PROJECT to have the choice of generating a project
+    # for packagin the library. Default is OFF since most people will not use this.
+    set( GENERATE_CPACK_PROJECT OFF CACHE BOOL "Decides if a cpack project should be generated. The project in the first loaded CMakeLists will configure CPack." )
+    mark_as_advanced( GENERATE_CPACK_PROJECT )
+  endif()
+  
+  if( setup_common_cache_var_PREFER_STATIC_LIBRARIES AND NOT DEFINED PREFER_STATIC_LIBRARIES )
+    # Add a cache variable PREFER_STATIC_LIBRARIES to have the choice of generating a project
+    # linking against static libraries if they exist. Default is OFF since most people will not use this.
+    set( PREFER_STATIC_LIBRARIES OFF CACHE BOOL "Decides if CMake should prefer static libraries to dynamic libraries when both exist." )
+    mark_as_advanced( PREFER_STATIC_LIBRARIES )
+
+    if( PREFER_STATIC_LIBRARIES )
+      set( CMAKE_FIND_LIBRARY_SUFFIXES .a;${CMAKE_FIND_LIBRARY_SUFFIXES} PARENT_SCOPE )  
+    endif()
+  endif()
+endfunction()
+
+# Sets up cache variable and other commands needed for a resource file for visual studio.
+# VERSION_PREFIX - The prefix is used to replace variables in the resource file cmake template.
+#                  Variables named <VERSION_PREFIX>_[MAJOR/MINOR/BUILD/SVN]_VERSION.
+# SVN_DIR_CANDIDATE - The directory which should be checked for whether it is a svn repository.
+# RESOURCE_FILE_CMAKE_TEMPLATE - The path to the cmake template of the resource file.
+# RESOURCE_FILE_OUTPUT_LOCATION - The location of the output resource file.
+# UPDATERESOURCEFILE_EXE - The location of UpdateResourceFile executable.
+# UPDATERESOURCEFILE_EXE_EXTRA_ARGS - Extra arguments that should be given to UpdateResourceFile in those
+# cases when the resource file has more info than just version.
+function( setupResourceFile target_name  )
+  # Add a cache variable ENABLE_SVN_REVISION to have the choice of using
+  # SubWCRev.exe to embed svn revision number in generated DLLs.
+  # Default is YES for Visual Studio and NMake generators, NO otherwise.
+  if( MSVC )
+    set( options )
+    set( oneValueArgs VERSION_PREFIX SVN_DIR_CANDIDATE RESOURCE_FILE_CMAKE_TEMPLATE RESOURCE_FILE_OUTPUT_LOCATION UPDATERESOURCEFILE_EXE )
+    set( multiValueArgs UPDATERESOURCEFILE_EXE_EXTRA_ARGS )
+    include( CMakeParseArguments )
+    cmake_parse_arguments( setup_resource_file "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+    
+    foreach( required_arg ${oneValueArgs} )
+      if( NOT DEFINED setup_resource_file_${required_arg} )
+        message( FATAL_ERROR "The required argument ${required_arg} is missing when calling setupResourceFile.")
+      endif()
+    endforeach()
+  
+    if( NOT DEFINED ENABLE_SVN_REVISION )
+      set( enable_svn_revision_default OFF )
+      if( CMAKE_GENERATOR MATCHES "Visual Studio|NMake" )
+        set( enable_svn_revision_default ON )
+      endif()
+      set( ENABLE_SVN_REVISION ${enable_svn_revision_default} CACHE BOOL "Use SubWCRev.exe to embed svn revision number in generated DLLs." )
+    endif()
+    
+    set( ${setup_resource_file_VERSION_PREFIX}_SVN_VERSION "0" )
+    if( ENABLE_SVN_REVISION )
+      # Find SubWCRev.exe
+      find_file( SubWCRev
+                 NAMES "SubWCRev.exe"
+                 DOC   "Set to SubWCRev.exe that comes with TortoiseSVN. Used to find svn revision number." )
+      mark_as_advanced( SubWCRev )
+    endif()
+
+    set( code_is_svn_working_copy 10 )
+    if( ENABLE_SVN_REVISION AND SubWCRev )
+      execute_process( COMMAND ${SubWCRev} ${setup_resource_file_SVN_DIR_CANDIDATE}
+                       RESULT_VARIABLE code_is_svn_working_copy )
+      if( ${code_is_svn_working_copy} EQUAL 0 )
+        set( ${setup_resource_file_VERSION_PREFIX}_SVN_VERSION "$WCREV$" )
+      endif()
+    endif()
+
+    # autogenerate the resource file depending on the version
+    configure_file( ${setup_resource_file_RESOURCE_FILE_CMAKE_TEMPLATE} ${setup_resource_file_RESOURCE_FILE_OUTPUT_LOCATION} )
+    
+    if( ENABLE_SVN_REVISION )
+      add_custom_command( TARGET ${target_name}
+                          PRE_BUILD
+                          COMMAND ${setup_resource_file_UPDATERESOURCEFILE_EXE}
+                          ARGS ${setup_resource_file_VERSION_PREFIX} ${setup_resource_file_RESOURCE_FILE_OUTPUT_LOCATION} ${setup_resource_file_RESOURCE_FILE_CMAKE_TEMPLATE}
+                          ${${setup_resource_file_VERSION_PREFIX}_MAJOR_VERSION} ${${setup_resource_file_VERSION_PREFIX}_MINOR_VERSION}
+                          ${${setup_resource_file_VERSION_PREFIX}_BUILD_VERSION} "${${setup_resource_file_VERSION_PREFIX}_SVN_VERSION}"
+                          ${setup_resource_file_UPDATERESOURCEFILE_EXE_EXTRA_ARGS}  )
+    endif()
+
+    if( ENABLE_SVN_REVISION AND SubWCRev AND ${code_is_svn_working_copy} EQUAL 0 )
+      # Update SVN revision in file.
+      execute_process( COMMAND ${SubWCRev} ${setup_resource_file_SVN_DIR_CANDIDATE} ${setup_resource_file_RESOURCE_FILE_OUTPUT_LOCATION} ${setup_resource_file_RESOURCE_FILE_OUTPUT_LOCATION} )
+
+      add_custom_command( TARGET ${target_name} 
+                          PRE_BUILD 
+                          COMMAND ${SubWCRev} 
+                          ARGS ${setup_resource_file_SVN_DIR_CANDIDATE} ${setup_resource_file_RESOURCE_FILE_OUTPUT_LOCATION} ${setup_resource_file_RESOURCE_FILE_OUTPUT_LOCATION} )
+    endif()
+  endif()
+endfunction()
