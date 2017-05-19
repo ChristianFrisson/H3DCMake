e7a74222691b364fd8b1bf6365e29c05fa4c5056
markus <markus@deaff870-4328-4c97-957a-1a6c77341feb>
2017-05-19 10:27:14 +0000

    Corrections to H3DExternalSearchPath.cmake include and deprecated message.
    T845
    
    git-svn-id: https://h3dapi.org:8090/CMake/trunk/CMake@111 deaff870-4328-4c97-957a-1a6c77341feb

--- H3DExternalSearchPath.cmake
+++ H3DExternalSearchPath.cmake
@@ -1,5 +1,6 @@
 # Contains a function which can be used to get default search paths
 # for include and lib directory on windows.
 
-message( AUTHOR_WARNING "H3DExternalSearchPath.cmake is deprecated. Change from include( H3DCommonFindModuleFunctions ) to include( H3DCommonFindModuleFunctions )." )
+message( AUTHOR_WARNING "H3DExternalSearchPath.cmake is deprecated. Change from include( H3DExternalSearchPath ) to include( H3DCommonFindModuleFunctions ) and you might also need include H3DUtilityFunctions." )
+include( H3DUtilityFunctions )
 include( H3DCommonFindModuleFunctions )
