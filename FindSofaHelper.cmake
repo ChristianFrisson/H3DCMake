edf7448b51218f70a6ace71af6bf9323f7ae522a
mike <mike@deaff870-4328-4c97-957a-1a6c77341feb>
2017-10-09 16:45:55 +0000

    Changed to handle sofa helper name mangling and different versions depending on vs version
    T1357
    
    git-svn-id: https://h3dapi.org:8090/CMake/trunk/CMake@128 deaff870-4328-4c97-957a-1a6c77341feb

--- FindSofaHelper.cmake
+++ FindSofaHelper.cmake
@@ -9,31 +9,34 @@
 include( H3DUtilityFunctions )
 
 if( WIN32 )
-  set( SOFA_LIBRARY_POSTFIX "_16_08" )
+  set( SOFA_LIBRARY_POSTFIX "_17_06" )
+  if( MSVC AND MSVC_VERSION LESS 1700 )
+    set( SOFA_LIBRARY_POSTFIX "_16_08" )
+  endif()
 endif()
 
 handleRenamingVariablesBackwardCompatibility( NEW_VARIABLE_NAMES SofaHelper_INCLUDE_DIR SofaHelper_LIBRARY_RELEASE SofaHelper_LIBRARY_DEBUG
                                               OLD_VARIABLE_NAMES SOFAHELPER_INCLUDE_DIR SOFAHELPER_LIBRARY SOFAHELPER_DEBUG_LIBRARY
                                               DOC_STRINGS "Path in which the file AdvancedTimer.h is located."
-                                                          "Path to SofaHelper${SOFA_LIBRARY_POSTFIX} library."
-                                                          "Path to SofaHelper${SOFA_LIBRARY_POSTFIX}d library." )
+                                                          "Path to SofaHelper${SOFA_LIBRARY_POSTFIX}_h3d library."
+                                                          "Path to SofaHelper${SOFA_LIBRARY_POSTFIX}d_h3d library." )
 
 include( H3DCommonFindModuleFunctions )
 get_filename_component( module_file_path ${CMAKE_CURRENT_LIST_FILE} PATH )
 getExternalSearchPathsH3D( module_include_search_paths module_lib_search_paths ${module_file_path} "sofahelper" "sofahelper/framework" )
 
 # Look for the header file.
-find_path( SofaHelper_INCLUDE_DIR NAMES   sofa/helper/AdvancedTimer.h
+find_path( SofaHelper_INCLUDE_DIR NAMES   sofa_h3d/helper/AdvancedTimer.h
                                   PATHS   ${module_include_search_paths}
                                   NO_DEFAULT_PATH 
                                   DOC     "Path in which the file AdvancedTimer.h is located." )
 # Look for the library.
-find_library( SofaHelper_LIBRARY_RELEASE   NAMES   SofaHelper${SOFA_LIBRARY_POSTFIX}
+find_library( SofaHelper_LIBRARY_RELEASE   NAMES   SofaHelper${SOFA_LIBRARY_POSTFIX}_h3d
                                    PATHS   ${module_lib_search_paths}
-                                   DOC     "Path to SofaHelper${SOFA_LIBRARY_POSTFIX} library." )
-find_library( SofaHelper_LIBRARY_DEBUG NAMES   SofaHelper${SOFA_LIBRARY_POSTFIX}d
+                                   DOC     "Path to SofaHelper${SOFA_LIBRARY_POSTFIX}_h3d library." )
+find_library( SofaHelper_LIBRARY_DEBUG NAMES   SofaHelper${SOFA_LIBRARY_POSTFIX}d_h3d
                                        PATHS   ${module_lib_search_paths}
-                                       DOC     "Path to SofaHelper${SOFA_LIBRARY_POSTFIX}d library." )
+                                       DOC     "Path to SofaHelper${SOFA_LIBRARY_POSTFIX}d_h3d library." )
                                        
 mark_as_advanced( SofaHelper_INCLUDE_DIR )
 mark_as_advanced( SofaHelper_LIBRARY_RELEASE )
