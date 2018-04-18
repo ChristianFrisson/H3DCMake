5a24a3efa11d02ca00114027b4a5db73d54eca83
hao <hao@deaff870-4328-4c97-957a-1a6c77341feb>
2018-04-18 19:41:30 +0000

    update: add FindNVAPI.cmake
    git-svn-id: https://h3dapi.org:8090/CMake/trunk/CMake@161 deaff870-4328-4c97-957a-1a6c77341feb

---
+++ FindNVAPI.cmake
@@ -0,0 +1,36 @@
+#  - Find the nvapi library
+#
+#  NVAPI_INCLUDE_DIRS
+#  NVAPI_LIBRARIES
+#  NVAPI_FOUND
+#
+#
+
+include( H3DUtilityFunctions )
+get_filename_component( module_file_path ${CMAKE_CURRENT_LIST_FILE} PATH  )
+getExternalSearchPathsH3D( module_include_search_paths module_lib_search_paths ${module_file_path} "nvapi" )
+
+# Look for the header file
+find_path( NVAPI_INCLUDE_DIRS NAMES nvapi.h
+           PATHS ${module_include_search_paths}
+           DOC "Path in which the file nvapi.h is located." )
+mark_as_advanced( NVAPI_INCLUDE_DIRS )
+
+getExternalSearchPathsH3D( module_include_search_paths module_lib_search_paths ${module_file_path} "static" )
+
+if ( WIN32 )
+  if( CMAKE_CL_64 )
+    set( LIBPOSTFIX "64" )
+  else( CMAKE_CL_64 )
+    set( LIBPOSTFIX "" )
+  endif( CMAKE_CL_64 )
+endif()
+# Look for the library.
+find_library( NVAPI_LIBRARIES NAMES  nvapi${LIBPOSTFIX}
+                          PATHS ${module_lib_search_paths}
+                          DOC "Path to nvapi library." )
+mark_as_advanced( NVAPI_LIBRARIES )
+
+include( FindPackageHandleStandardArgs )
+find_package_handle_standard_args( NVAPI DEFAULT_MSG
+                                   NVAPI_LIBRARIES NVAPI_INCLUDE_DIRS )
