b6d7c674940cf1dcd46cd3e4bb9eefbca8f047e1
hao <hao@deaff870-4328-4c97-957a-1a6c77341feb>
2018-04-23 11:48:36 +0000

    update: provide NVAPI_INSTALL_DIR for users to provide their local nvapi library to be used.
    git-svn-id: https://h3dapi.org:8090/CMake/trunk/CMake@162 deaff870-4328-4c97-957a-1a6c77341feb

--- FindNVAPI.cmake
+++ FindNVAPI.cmake
@@ -10,24 +10,31 @@ include( H3DUtilityFunctions )
 get_filename_component( module_file_path ${CMAKE_CURRENT_LIST_FILE} PATH  )
 getExternalSearchPathsH3D( module_include_search_paths module_lib_search_paths ${module_file_path} "nvapi" )
 
+
 # Look for the header file
 find_path( NVAPI_INCLUDE_DIRS NAMES nvapi.h
-           PATHS ${module_include_search_paths}
+           PATHS ${NVAPI_INSTALL_DIR}
+                 ${module_include_search_paths}
            DOC "Path in which the file nvapi.h is located." )
 mark_as_advanced( NVAPI_INCLUDE_DIRS )
+set(NVAPI_INSTALL_DIR "" CACHE PATH "NVAPI_INSTALL_DIR")
 
 getExternalSearchPathsH3D( module_include_search_paths module_lib_search_paths ${module_file_path} "static" )
 
 if ( WIN32 )
   if( CMAKE_CL_64 )
+    set( sdk_lib "amd64" )
     set( LIBPOSTFIX "64" )
   else( CMAKE_CL_64 )
+    set( sdk_lib "x86" )
     set( LIBPOSTFIX "" )
   endif( CMAKE_CL_64 )
 endif()
 # Look for the library.
 find_library( NVAPI_LIBRARIES NAMES  nvapi${LIBPOSTFIX}
-                          PATHS ${module_lib_search_paths}
+                          PATHS ${NVAPI_INSTALL_DIR}/${sdk_lib}
+                                ${module_lib_search_paths}
+                                
                           DOC "Path to nvapi library." )
 mark_as_advanced( NVAPI_LIBRARIES )
 
