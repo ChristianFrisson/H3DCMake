38c62cd1d072b7cea78235d7fefc13bfffbf451e
mike <mike@deaff870-4328-4c97-957a-1a6c77341feb>
2018-06-01 09:33:40 +0000

    ADD: Added cmake find module for PerfWorks T2002
    git-svn-id: https://h3dapi.org:8090/CMake/trunk/CMake@168 deaff870-4328-4c97-957a-1a6c77341feb

---
+++ FindPerfWorks.cmake
@@ -0,0 +1,34 @@
+#  - Find the PerfWorks library
+#
+#  PerfWorks_INCLUDE_DIRS
+#  PerfWorks_FOUND
+#  PerfWorks_LIBRARIES
+#
+#
+
+include( H3DUtilityFunctions )
+
+set(PerfWorks_INSTALL_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../../PerfWorks" CACHE PATH "PerfWorks_INSTALL_DIR")
+# Look for the header file
+find_path( PerfWorks_INCLUDE_DIRS NAMES nvperfapi.h
+           PATHS ${PerfWorks_INSTALL_DIR}/include
+           DOC "Path in which the file nvperfapi.h is located." )
+mark_as_advanced( PerfWorks_INCLUDE_DIRS )
+
+if ( WIN32 )
+  if( CMAKE_CL_64 )
+    set( sdk_lib "x64" )
+    set( LIBPOSTFIX "64" )
+  else( CMAKE_CL_64 )
+    set( sdk_lib "x86" )
+    set( LIBPOSTFIX "32" )
+  endif( CMAKE_CL_64 )
+endif()
+
+# Look for the library.
+find_library( PerfWorks_LIBRARIES NAMES nvperfapi${LIBPOSTFIX}
+                                  PATHS ${PerfWorks_INSTALL_DIR}/bin/${sdk_lib} )
+
+include( FindPackageHandleStandardArgs )
+find_package_handle_standard_args( PerfWorks DEFAULT_MSG
+                                   PerfWorks_INCLUDE_DIRS PerfWorks_INCLUDE_DIRS )
